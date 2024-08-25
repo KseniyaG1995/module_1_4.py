face9ae69bf248509ce29beb0f522e9b5ba2ecf9
KseniyaG1995 <143842643+KseniyaG1995@users.noreply.github.com>
2024-08-26 01:09:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#1
+my_string = input('Привет!: ')
+print(len(my_string))
+#2
+print(my_string.upper())
+print(my_string.lower())
+print('Добро пожаловать, '.replace(' ','') + my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
